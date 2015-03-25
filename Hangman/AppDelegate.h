537ac27030c953c873115f739f48366f69dfb940
Eoin Morgan <eoinmorgan@berkeley.edu>
2015-03-24 18:41:58 -0700

    Initial Commit

---
+++ Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by Eoin Morgan on 3/24/15.
+//  Copyright (c) 2015 Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
