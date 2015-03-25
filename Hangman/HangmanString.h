db56f7f9944518e0fff999fcbf216f5decbd7356
Eoin Morgan <eoinmorgan@berkeley.edu>
2015-03-24 18:46:51 -0700

    added new class file for hangman string representation

---
+++ Hangman/HangmanString.h
@@ -0,0 +1,13 @@
+//
+//  HangmanString.h
+//  Hangman
+//
+//  Created by Eoin Morgan on 3/24/15.
+//  Copyright (c) 2015 Morgan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HangmanString : NSObject
+
+@end
---
+++ Hangman/HangmanString.m
@@ -0,0 +1,13 @@
+//
+//  HangmanString.m
+//  Hangman
+//
+//  Created by Eoin Morgan on 3/24/15.
+//  Copyright (c) 2015 Morgan. All rights reserved.
+//
+
+#import "HangmanString.h"
+
+@implementation HangmanString
+
+@end
