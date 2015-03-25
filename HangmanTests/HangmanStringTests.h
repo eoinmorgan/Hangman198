0f12605cedee020cbe01578e571b16ae1f63707c
Eoin Morgan <eoinmorgan@berkeley.edu>
2015-03-24 21:38:16 -0700

    added HangmanString tests; decided to allow case-sensitivity for ease of implementation.

---
+++ HangmanTests/HangmanStringTests.h
@@ -0,0 +1,13 @@
+//
+//  HangmanStringTests.h
+//  Hangman
+//
+//  Created by Eoin Morgan on 3/24/15.
+//  Copyright (c) 2015 Morgan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HangmanStringTests : XCTest
+
+@end
---
+++ HangmanTests/HangmanStringTests.m
@@ -0,0 +1,53 @@
+//
+//  HangmanStringTests.m
+//  Hangman
+//
+//  Created by Eoin Morgan on 3/24/15.
+//  Copyright (c) 2015 Morgan. All rights reserved.
+//
+
+#import "HangmanString.h"
+#import <XCTest/XCTest.h>
+
+//
+//  HangmanStringTests.h
+//  Hangman
+//
+//  Created by Eoin Morgan on 3/24/15.
+//  Copyright (c) 2015 Morgan. All rights reserved.
+//
+
+@interface HangmanStringTests : XCTestCase
+
+@end
+
+@implementation HangmanStringTests
+
+- (void)testDoesContainLetter {
+    NSString *str = @"TEST STRING";
+    HangmanString *hs = [[HangmanString alloc] initWithString:str];
+    BOOL result;
+    
+    result = [hs doesContainLetter:@"T"];
+    XCTAssertTrue(result);
+    
+    result = [hs doesContainLetter:@"Q"];
+    XCTAssertFalse(result);
+}
+
+- (void)testGetDisplayForGuesses {
+    NSString *str = @"TEST STRING";
+    HangmanString *hs = [[HangmanString alloc] initWithString:str];
+    
+    NSArray *guesses1 = @[@"T", @"S", @"G"];
+    NSString *display1 = [hs getDisplayForGuesses:guesses1];
+    NSString *expected1 = @"T?ST ST???G";
+    XCTAssertTrue([display1 isEqualToString:expected1]);
+    
+    NSArray *guesses2 = @[@"E", @"I", @"N"];
+    NSString *display2 = [hs getDisplayForGuesses:guesses2];
+    NSString *expected2 = @"?E?? ???IN?";
+    XCTAssertTrue([display2 isEqualToString:expected2]);
+}
+
+@end
